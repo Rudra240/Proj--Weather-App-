3498851c355e7bfa895d09cb3a0fc70ac05dd539
Rudra240 <136898287+Rudra240@users.noreply.github.com>
2023-08-15 23:22:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const apiKey="f4e2f10a3c329000cc55e3342f7af6dd";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox =document.querySelector(".search input");
+const searchBtn =document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkweather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+    
+    if(response.status== 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }else{
+     
+        var data = await response.json();
+
+
+    
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+ "%";
+    document.querySelector(".wind").innerHTML=data.wind.speed +"km/h";
+    
+    
+    switch (data.weather[0].main) {
+        case "Clouds":
+            weatherIcon.src="images/clouds.png";
+            break;
+
+        case "Clear":
+            weatherIcon.src="images/clear.png";
+            break;
+
+        case "Rain":
+            weatherIcon.src="images/rain.png";
+            break;
+
+        case "Drizzle":
+            weatherIcon.src="images/drizzle.png";
+             break;
+        
+        case "Mist":
+            weatherIcon.src="images/mist.png";
+             break;
+
+        default:
+            break;
+    }
+
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+    }
+    
+}
+
+
+searchBtn.addEventListener("click", ()=>{
+    checkweather(searchbox.value);
+});
